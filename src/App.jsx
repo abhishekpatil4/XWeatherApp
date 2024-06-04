a8b2378804fd290a35de4ab75b308f92c4f2aac2
Abhishek Patil <abishkpatil@gmail.com>
2024-06-04 21:10:12 +0530

    fix: minor bugs to pass testcase

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
       <button type='submit'>Search</button>
     </form>
     {loading ?
-      <p>Loading data…</p>
+      <p>Loading data...</p>
       :
       cityData.condition !== ""
       &&
