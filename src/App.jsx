7fa5277916ece5216ed69c40262a71c98ff5092a
Abhishek Patil <abishkpatil@gmail.com>
2024-06-04 19:47:50 +0530

    feat: add function to fetch API data

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [cityData, setCityData] = useState({
+    temp: "",
+    humidity: "",
+    condition: "",
+    windSpeed: ""
+  })
+  const fetchCityData = async (city) => {
+    const API_KEY = "47a0640b15414677bab100536242303"
+    const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
+    try {
+      const res = await axios.get(url);
+      console.log("res: ", res.data);
+      setCityData({
+        temp: res?.data.current.temp_c,
+        humidity: res?.data.current.humidity,
+        condition: res?.data.current.condition.text,
+        windSpeed: res?.data.current.wind_kph
+      })
+
+    } catch (error) {
+      if (error.response.status === 400) {
+        alert("Failed to fetch weather data");
+      }
+      console.log(error);
+    }
+
+  }
   const handleSearch = (event) => {
     event.preventDefault();
     const data = new FormData(event.target);
     const payload = Object.fromEntries(data);
-    console.log("payload: ", payload);
+    fetchCityData(payload.cityName);
   }
   return <div>
     <form onSubmit={handleSearch} style={{ display: 'flex', alignItems: 'center' }}>
