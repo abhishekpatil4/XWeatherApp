05435194fa17c95b54e31a2aadaca7594b6143fa
Abhishek Patil <abishkpatil@gmail.com>
2024-06-04 19:26:21 +0530

    feat: add input and search button

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const payload = Object.fromEntries(data);
+    console.log("payload: ", payload);
+  }
+  return <div>
+    <form onSubmit={handleSearch} style={{ display: 'flex', alignItems: 'center' }}>
+      <input required name="cityName" type="text" placeholder='Enter city name' style={{ height: '2rem', paddingLeft: '1rem', minWidth: '20rem' }} />
+      <button type='submit'>Search</button>
+    </form>
+  </div>
 }
 
 export default App
